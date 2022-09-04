6c4c5ab0e5b8ba27bc8a9d6917d30dc7fd57d670
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-09-04 09:52:08 -0500

    adding a useritem component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -4,13 +4,14 @@ import UsersList from '../components/UsersList'
 
 const Users = () => {
     const USERS = [
-        {
-            id: 'u1', 
-            name: 'Jenni Ferguson', 
-            image: 'https://teamsmile.org/about-jenni-ferguson/', 
-            places: 3 
-        }
-    ];
+      {
+        id: 'u1',
+        name: 'Jenni Ferguson',
+        image:
+          'https://teamsmile.org/wp-content/uploads/2017/06/Jenni_0032_Color-250x300.jpg',
+        places: 3,
+      },
+    ]
   return <UsersList  items={USERS}/>
   
 }
