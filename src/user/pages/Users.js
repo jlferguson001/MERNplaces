bd6c586736f1e3fe11f810fdcfa84d984c91c2c2
Jenni Ferguson <98863330+jlferguson001@users.noreply.github.com>
2022-09-04 08:48:56 -0500

    adding a userslist page/components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
+import UsersList from '../components/UsersList'
+
 const Users = () => {
-  return( 
-  <h2> Users Works</h2>
-)}
+    const USERS = [
+        {
+            id: 'u1', 
+            name: 'Jenni Ferguson', 
+            image: 'https://teamsmile.org/about-jenni-ferguson/', 
+            places: 3 
+        }
+    ];
+  return <UsersList  items={USERS}/>
+  
+}
 
 export default Users
